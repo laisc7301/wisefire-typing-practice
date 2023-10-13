cfdc08ea35d85132793c5287cf43ff50c0eb0191
laisc(home) <laisc7301@gmail.com>
2023-10-13 11:02:49 +0800

    n

--- js/replacehhf.js
+++ js/replacehhf.js
@@ -8,8 +8,3 @@ function replacebrtohhf(value) {
 	let str = value.replace(/<br\/>/g, "\n");
 	return str;
 }
-
-function replacehhfToN(value) {
-	let str = value.replace(/\r\n|\r|\n/g, "\n");
-	return str;
-}
